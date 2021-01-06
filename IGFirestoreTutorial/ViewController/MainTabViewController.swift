ecee58cec736d3d6bf9f14aac8c4553bda146ca8
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2021-01-06 17:26:48 +0000

    1. Configure Project

---
+++ IGFirestoreTutorial/ViewController/MainTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabViewController.swift
+//  IGFirestoreTutorial
+//
+//  Created by Daniel Sutcliffe on 06/01/2021.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
