7946fe6ac14c30249414fce64df76640bbabe6b0
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2021-01-06 17:57:01 +0000

    3. Finish Tab Bar Setup

--- IGFirestoreTutorial/ViewController/MainTabViewController.swift
+++ IGFirestoreTutorial/ViewController/MainTabViewController.swift
@@ -23,16 +23,25 @@ class MainTabViewController: UITabBarController {
         
         view.backgroundColor = .white
         
-        let feed = FeedViewController()
-        let search = SearchViewController()
-        let create = CreatePostViewController()
-        let notifications = NotificationViewController()
-        let profile = ProfileViewController()
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedViewController())
+        let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchViewController())
+        let create = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: CreatePostViewController())
+        let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationViewController())
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileViewController())
         
         /// viewControllers is an Array of root view controllers that will be displayed in the tab bar
         /// inherited from UITabBarController
         viewControllers = [feed, search, create, notifications, profile]
+        tabBar.tintColor = .black
         
+    }
+    
+    func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
     }
     
 }
