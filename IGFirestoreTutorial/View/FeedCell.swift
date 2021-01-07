b89a6c2e2a1e9171db01f5452baf1c816ab4a47b
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2021-01-07 17:10:15 +0000

    5. Feed Cell UI & Intro To Programmatic Constraints

---
+++ IGFirestoreTutorial/View/FeedCell.swift
@@ -0,0 +1,60 @@
+//
+//  FeedCell.swift
+//  IGFirestoreTutorial
+//
+//  Created by Daniel Sutcliffe on 07/01/2021.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        
+        return iv
+    }()
+    
+    /// Not done until initialisation has happened, If wasnt lazy it would have to go in init
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    // MARK: Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(usernameButton)
+        usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Actions
+    
+    @objc func didTapUsername() {
+        print("DEBUG: did tap username")
+    }
+}
