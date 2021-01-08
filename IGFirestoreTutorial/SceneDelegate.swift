b073ed191765ec49ac02fd85fae2b1ec0dbe6e2f
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2021-01-08 15:12:03 +0000

    7. Login Page UI

--- IGFirestoreTutorial/SceneDelegate.swift
+++ IGFirestoreTutorial/SceneDelegate.swift
@@ -21,7 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        guard let _ = (scene as? UIWindowScene) else { return }
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = MainTabViewController()
+//        window?.rootViewController = MainTabViewController()
+        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window?.makeKeyAndVisible()
     }
 
---
+++ IGFirestoreTutorial/ViewController/Authentication/RegistrationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationViewController.swift
+//  IGFirestoreTutorial
+//
+//  Created by Daniel Sutcliffe on 08/01/2021.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Lifecysle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+    }
+}
